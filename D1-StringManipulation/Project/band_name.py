0fbd30af7661e006d4639420c7fca078fbf2371d
Faiyaz Jambotkar <110715525+FaiyazJambotkar@users.noreply.github.com>
2023-03-23 10:36:28 +0530

    Delete D1-StringManipulation directory

--- D1-StringManipulation/Project/band_name.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator")
-print("Hello, " + input("What is your name?\n") + "!")
-
-city_name = input("Which city did you grow up in?\n")
-pet_name = input("What is the name of your pet?\n")
-
-print("\nYour band name could be " + city_name + " " + pet_name + "!")
